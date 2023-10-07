10383afa18201c0dca99f0dbf81e1914601174b8
maxence <deathloadtv@gmail.com>
2023-10-07 10:20:03 +0700

    New save / New like

---
+++ app/src/main/java/vn/edu/usth/wordpress25/ui/Reader/Following_example_2.java
@@ -0,0 +1,25 @@
+package vn.edu.usth.wordpress25.ui.Reader;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import vn.edu.usth.wordpress25.R;
+
+public class Following_example_2 extends Fragment {
+
+    private View rootview;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootview =  inflater.inflate(R.layout.fragment_following_example_2, container, false);
+        return rootview;
+    }
+
+
+}
